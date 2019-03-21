3a6f610b0464cadbd87ee4cd0a76e99b7657eacf
manny <mannyt@timeportal.io>
2019-03-20 21:33:05 -0400

    Added investment guides.

---
+++ Assignment3_S19/Controllers/GuidesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment3_S19.Controllers
+{
+    public class GuidesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Equity()
+        {
+            return View();
+        }
+
+        public ActionResult FixedIncome()
+        {
+            return View();
+        }
+
+        public ActionResult MutualFunds()
+        {
+            return View();
+        }
+
+        public ActionResult OtherServices()
+        {
+            return View();
+        }
+    }
+}
