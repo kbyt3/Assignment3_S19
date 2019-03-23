b892f61e9ea0de1013ca75a3a0b5cf23dfa9ca27
manny <mannyt@timeportal.io>
2019-03-22 20:28:05 -0400

    Improvement to dashboard and added stock details page.

---
+++ Assignment3_S19/Controllers/QuotesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Assignment3_S19.Services;
+using Assignment3_S19.Models;
+
+
+namespace Assignment3_S19.Controllers
+{
+    public class QuotesController : Controller
+    {
+        private IEXTradingService _iexService;
+
+        public QuotesController(IEXTradingService iexService)
+        {
+            _iexService = iexService;
+        }
+
+        public async Task<ActionResult> Index(string symbol, string range = "1m")
+        {
+            var response = await _iexService.GetStockData(
+                symbols: new string[] { symbol },
+                data: new string[] { "quote", "company", "chart", "news", "logo" },
+                range: range);
+
+            IEXApiResponse model = response.GetValueOrDefault(symbol);
+
+            ViewData["range"] = range;
+
+            return View(model);
+        }
+    }
+}
---
+++ Assignment3_S19/Helpers/DateHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3_S19.Helpers
+{
+    public static class DateHelper
+    {
+        public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+        {
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+            dt = dt.AddMilliseconds(unixTimeStamp).ToLocalTime();
+
+            return dt;
+        }
+    }
+}
